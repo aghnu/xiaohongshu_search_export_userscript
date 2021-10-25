82404daad329e25b9fc2a7fb8203bec8776ad372
Gengyuan Huang <gengyuan@ualberta.ca>
2021-10-25 13:25:54 -0600

    auto next page working, content.append working

--- xhs.js
+++ xhs.js
@@ -17,31 +17,54 @@
     var targetURL = "cooperator/blogger/v2";
 	var sheetName = "Test Sheet";
     var ifstart = false;
+    var gotinfo = false;
     var content = [];
 	var nextPage = () => {};
-	var workbook = createWorkBook(sheetName);
+	// var workbook = createWorkBook(sheetName);
 
 
     // set information process function
     const processor = (responseBodyText) => {
 		if (!ifstart) {
 			content = [responseBodyText];
+            gotinfo = true;
 		} else {
-			content = content.push(responseBodyText);
-			nextPage();
+			content.push(responseBodyText);
+			gotinfo = true;
 		}
     }
 
-	const createWorkBook = (sheetname, props={}) => {
-		var wb = XLSX.utils.book_new();
-		wb.Props = {};
-		wb.SheetNames.push(sheetname)
-		return wb;
-	}
+    const clickStart = () => {
+        console.log("clicked");
+        ifstart = true;
+        waitsForNext();     // set auto next page
+    }
 
-	const addNewRowToWB = (wb) => {
+	// const createWorkBook = (sheetname, props={}) => {
+	// 	var wb = XLSX.utils.book_new();
+	// 	wb.Props = {};
+	// 	wb.SheetNames.push(sheetname)
+	// 	return wb;
+	// }
 
-	}
+	// const addNewRowToWB = (wb) => {
+
+	// }
+
+    const waitsForNext = () => {
+        // this function looking for the next button, when next button open, click the button
+        let countdown = setInterval(() => {
+
+            let nextButton = document.getElementsByClassName("pagination_cell")[8];
+            console.log("next");
+            console.log(gotinfo);
+            console.log(nextButton);
+            if ((gotinfo)&&(nextButton != null)) {
+                gotinfo = false;
+                nextButton.click();
+            }
+        }, 1000);
+    }
 
     // injection
     // intercepting AJAX responses
@@ -56,34 +79,46 @@
       send.apply(this, arguments);
     };
 
+
+
+    let buttonStyle = `
+        z-index: 999;
+        background-color: white;
+        border: solid;
+        border-color: black;
+        color: black;
+        padding: 0.3em;
+        font-size: 1.2em;
+        font-weight: bold;
+        border-radius: 0.3em;
+    `
+
     // create button and onclick listener
     let trigger = document.createElement('button');
-    trigger.setAttribute(
+    trigger.setAttribute('style',buttonStyle);
+    trigger.innerHTML = "Click here to start";
+    trigger.addEventListener('click', clickStart);
+
+    let printer = document.createElement('button');
+    printer.setAttribute('style',buttonStyle);
+    printer.innerHTML = "Click here to download";
+    printer.addEventListener('click', clickStart);
+
+    let controlPanel = document.createElement('div');
+    controlPanel.setAttribute(
         'style',
         `
+            display: flex;
+            gap: 1em;
             position: fixed;
             bottom: 2em;
-            right: 2em;
-            z-index: 999;
-            background-color: black;
-            color: white;
-            padding: 0.5em;
-            font-size: 1.5em;
-            font-weight: bolder;
-            border-radius: 0.5em;
+            left: 2em;
         `
-    );
-    trigger.innerHTML = "Click here to start";
-    trigger.addEventListener('click', () => {
+    )
 
-		if (!ifstart) {
-			let nextButton = document.getElementsByClassName("pagination_cell")[8];
-			ifstart = true;
-			nextPage = nextButton.click;
-		}
+    controlPanel.appendChild(trigger);
+    controlPanel.appendChild(printer);
 
-    });
-
-    document.body.appendChild(trigger);
+    document.body.appendChild(controlPanel);
     // document.body.appendChild(inject);
 })();
